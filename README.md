c274258de7beacea64cba9f11ad3d1f588770d84
TienM <tienleigh@yahoo.com>
2016-12-16 20:05:51 -0600

    Added error page.

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Lab
 
 A personal dictionary and repository for tutorial and resource links.
+
 Link: https://bootd.herokuapp.com
--- server.js
+++ server.js
@@ -3,11 +3,39 @@ var app      = express();
 var mongoose = require('mongoose');
 var flash    = require('connect-flash');
 
+mongoose.connect(process.env.MONGO_URL);
+
 app.set('view engine', 'ejs');
 app.use(flash()); //for flash messages stored in session
 
 require('./routes/routes.js')(app);
 
+
 app.listen(3000, function() {
   console.log("The magic happens on port 3000!");
+});
+
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+//production error handler, no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
