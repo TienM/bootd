7fef1b48c758ddf6e1c081b2788713954ce05acd
TienM <tienleigh@yahoo.com>
2017-01-18 17:27:40 -0600

    Testing

--- routes/terms.js
+++ routes/terms.js
@@ -1,15 +1,8 @@
 var express = require('express');
     router  = express.Router();
-    db = mongojs(dbconfig, ['terms']);
+    db = mongoose(dbconfig, ['terms']);
 
 //All Tasks
-router.get('/terms', function(req, res, next){
-  db.terms.find(function(err, terms){
-    if (err) {
-      res.send(err);
-    }
-    res.json(terms);
-  });
-});
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,18 +1,30 @@
-var express    = require('express');
-    bodyParser = require('body-parser');
-    hbs        = require('express-handlebars');
-    app        = express();
+var express    = require('express'),
+    app        = express(),
+    path       = require('path'),
+    hbs        = require('express-handlebars'),
+    morgan     = require('morgan'),
+    mongoose   = require('mongoose');
 
-app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts'}));
-app.set('view engine', 'hbs');
+//App Config
+  //Views
+  app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts'}));
+  app.set('view engine', 'hbs');
 
-require('./routes/routes.js')(app);
-
-
-app.listen(process.env.PORT || 3000, function() {
-  console.log("The magic happens on port 3000!");
+  //Connect to Database
+  mongoose.connect('mongodb://TienM:rain01@ds143608.mlab.com:43608/bootd');
+  //Routes
+  require('./routes/routes.js')(app);
+  //configure app to handle CORS requests
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+  next();
 });
 
+  app.use(morgan('dev'));
+
+//Error Stack
 app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
@@ -36,4 +48,9 @@ app.use(function(err, req, res, next) {
     message: err.message,
     error: {}
   });
+});
+module.exports = app;
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("The magic happens on port 3000!");
 });
